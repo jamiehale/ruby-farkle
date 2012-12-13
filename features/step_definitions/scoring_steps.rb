b59ad2090d35b7246a952113fbb26d1a378bfd37
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-13 18:24:12 -0500

    Add default feature steps (pending)
    (cucumber passes with pending steps)

---
+++ features/step_definitions/scoring_steps.rb
@@ -0,0 +1,15 @@
+Given /^the player is not open$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the roll "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the scorer is run$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the scores should include "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
