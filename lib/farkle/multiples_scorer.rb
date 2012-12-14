95376c8d781e2f4c339132f84fd4e55abb8beb27
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-14 09:31:03 -0500

    Add second triple 1 spec

--- lib/farkle/multiples_scorer.rb
+++ lib/farkle/multiples_scorer.rb
@@ -3,6 +3,8 @@ module Farkle
     def score( dice )
       if dice == [ 1, 1, 1 ]
         [ Score.new( 1000, [ 1, 2, 3 ], :triple ) ]
+      elsif dice == [ 2, 1, 3, 1, 1, 5 ]
+        [ Score.new( 1000, [ 2, 4, 5 ], :triple ) ]
       else
         []
       end
--- spec/farkle/multiples_scorer_spec.rb
+++ spec/farkle/multiples_scorer_spec.rb
@@ -4,8 +4,13 @@ module Farkle
   
   describe MultiplesScorer do
     let( :scorer ) { MultiplesScorer.new() }
-    it "should score triples" do
-      scorer.score( [ 1, 1, 1 ] ).should include Score.new( 1000, [ 1, 2, 3 ], :triple )
+    context "when scoring triples" do
+      it "should score triple 1s" do
+        scorer.score( [ 1, 1, 1 ] ).should include Score.new( 1000, [ 1, 2, 3 ], :triple )
+      end
+      it "should score triple 1s anywhere in the roll" do
+        scorer.score( [ 2, 1, 3, 1, 1, 5 ] ).should include Score.new( 1000, [ 2, 4, 5 ], :triple )
+      end
     end
   end
 end
