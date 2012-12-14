aa67d75c2aa8ec426d9e08b9387c6fe6e46135a6
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-14 17:32:07 -0500

    Refactor SinglesScorer for clarity

--- lib/farkle/singles_scorer.rb
+++ lib/farkle/singles_scorer.rb
@@ -3,16 +3,24 @@ module Farkle
   class SinglesScorer
     
     def score( dice )
-      dice.each_with_index.inject( [] ) do |scores,(roll,index)|
-        if roll == 1
-          scores + [ Score.new( 100, [ index + 1 ], :single ) ]
-        elsif roll == 5
-          scores + [ Score.new( 50, [ index + 1 ], :single ) ]
-        else
-          scores
+      score_singles( 1, dice ) + score_singles( 5, dice )
+    end
+
+    private
+    
+      def score_singles( pips, dice )
+        dice.each_with_index.inject( [] ) do |scores,(roll,index)|
+          roll == pips ? scores + [ Score.new( score_from_pips( pips ), [ index + 1 ], :single ) ] : scores
         end
       end
-    end
+      
+      def score_from_pips( pips )
+        if pips == 1
+          100
+        else
+          50
+        end
+      end
     
   end
   
