1373fa6def78e2d5a2c62150926327754063df4b
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-13 23:14:08 -0500

    Added all working open scoring specs
    (up to triple 1s)

--- spec/farkle/scorer_spec.rb
+++ spec/farkle/scorer_spec.rb
@@ -121,9 +121,88 @@ module Farkle
       end
     end
     context "when open" do
-      it "should score singles"
-      it "should recognize farkles"
-      it "should score multiples"
+      let( :scorer ) { Scorer.new( :open ) }
+      describe "singles" do
+        it "should score single 1s" do
+          scorer.score( [ 1 ] ).should include "100:1"
+        end
+        it "should score single 1s at other positions" do
+          scorer.score( [ 2, 2, 1 ] ).should include "100:3"
+        end
+        context "when roll includes multiple 1s" do
+          let( :scores ) { scorer.score( [ 1, 2, 1, 4, 1 ] ) }
+          it "should score first 1" do
+            scores.should include "100:1"
+          end
+          it "should score second 1" do
+            scores.should include "100:3"
+          end
+          it "should score third 1" do
+            scores.should include "100:5"
+          end
+        end
+        it "should score single 5s" do
+          scorer.score( [ 5 ] ).should include "50:1"
+        end
+        it "should score single 5s at other positions" do
+          scorer.score( [ 3, 3, 4, 5 ] ).should include "50:4"
+        end
+        context "when roll includes multiple 5s" do
+          let( :scores ) { scorer.score( [ 2, 5, 5, 3, 4, 5 ] ) }
+          it "should score first 5" do
+            scores.should include "50:2"
+          end
+          it "should score second 5" do
+            scores.should include "50:3"
+          end
+          it "should score third 5" do
+            scores.should include "50:6"
+          end
+        end
+        context "when roll includes both 1s and 5s" do
+          let( :scores ) { scorer.score( [ 1, 5, 2, 5, 1, 1 ] ) }
+          it "should score first 1" do
+            scores.should include "100:1"
+          end
+          it "should score first 5" do
+            scores.should include "50:2"
+          end
+          it "should score second 5" do
+            scores.should include "50:4"
+          end
+          it "should score second 1" do
+            scores.should include "100:5"
+          end
+          it "should score third 1" do
+            scores.should include "100:6"
+          end
+        end
+      end
+      context "when scoring farkles" do
+        it "should recognize 1-die farkles" do
+          scorer.score( [ 2 ] ).should be_empty
+        end
+        it "should recognize 2-die farkles" do
+          scorer.score( [ 3, 4 ] ).should be_empty
+        end
+        it "should recognize 3-die farkles" do
+          scorer.score( [ 6, 2, 3 ] ).should be_empty
+        end
+        it "should recognize 4-die farkles" do
+          scorer.score( [ 2, 3, 3, 2 ] ).should be_empty
+        end
+        it "should recognize 5-die farkles" do
+          scorer.score( [ 3, 4, 6, 6, 4 ] ).should be_empty
+        end
+        it "should recognize 6-die farkles" do
+          scorer.score( [ 2, 4, 3, 6, 4, 3 ] ).should be_empty
+        end
+      end
+      context "when scoring multiples" do
+        it "should score triple 1s" do
+          scorer.score( [ 1, 1, 1 ] ).should include "1000:1,2,3"
+        end
+      end
       it "should score straights"
       it "should score triple-doubles"
       it "should score double-triples"
