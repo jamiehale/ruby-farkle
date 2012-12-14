c6c2488e275375e81fb3d76c25a83e558d55abcb
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-14 09:11:21 -0500

    Add specs for Score

--- lib/farkle/score.rb
+++ lib/farkle/score.rb
@@ -1,4 +1,12 @@
 module Farkle
   class Score
+    attr_reader :value
+    attr_reader :dice_indices
+    attr_reader :type
+    def initialize( value, dice_indices, type )
+      @value = value
+      @dice_indices = dice_indices
+      @type = type
+    end
   end
 end
--- spec/farkle/score_spec.rb
+++ spec/farkle/score_spec.rb
@@ -2,8 +2,15 @@ require File.join( File.dirname( __FILE__ ), "../rspec_helper" )
 
 module Farkle
   describe Score do
-    it "should instantiate" do
-      Score.new()
+    let( :score ) { Score.new( 1000, [ 1, 2, 3 ], :triple ) }
+    it "should have a value" do
+      score.value.should == 1000
+    end
+    it "should have an array of dice" do
+      score.dice_indices.should == [ 1, 2, 3 ]
+    end
+    it "should have a type" do
+      score.type.should == :triple
     end
   end
 end
