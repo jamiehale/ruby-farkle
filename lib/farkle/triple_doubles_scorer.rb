4af484e2eb4caff79dd333d8a9d6cf07b884f445
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-14 15:45:08 -0500

    Refactor TripleDoublesScorer for generic case

--- lib/farkle/triple_doubles_scorer.rb
+++ lib/farkle/triple_doubles_scorer.rb
@@ -1,11 +1,19 @@
 module Farkle
   class TripleDoublesScorer
     def score( dice )
-      if dice.sort == [ 1, 1, 2, 2, 3, 3 ]
+      if is_triple_double?( dice )
         [ Score.new( 1500, [ 1, 2, 3, 4, 5, 6 ], :triple_double ) ]
       else
         []
       end
     end
+    private
+      def is_triple_double?( dice )
+        rolls = [ 0, 0, 0, 0, 0, 0 ]
+        dice.each do |roll|
+          rolls[ roll - 1 ] += 1
+        end
+        rolls.count( 2 ) == 3
+      end
   end
 end
