2fdb5fa11cd6c3e0d687f40cb875232c16352319
Jamie Hale <jamie@smallarmyofnerds.com>
2012-12-13 23:18:53 -0500

    Refactored Scorer to make class extraction easier

--- lib/farkle/scorer.rb
+++ lib/farkle/scorer.rb
@@ -5,17 +5,30 @@ module Farkle
     end
     def score( dice )
       results = []
-      dice.each_with_index do |roll,index|
-        if roll == 1
-          results << "100:#{index+1}"
-        elsif roll == 5
-          results << "50:#{index+1}"
-        end
-      end
-      if @state == :open and dice == [ 1, 1, 1 ]
-        results << "1000:1,2,3"
-      end
+      results += score_singles( dice )
+      results += score_multiples( dice )
       results
     end
+    
+    private
+      def score_singles( dice )
+        dice.each_with_index.inject( [] ) do |scores,(roll,index)|
+          if roll == 1
+            scores + [ "100:#{index+1}" ]
+          elsif roll == 5
+            scores + [ "50:#{index+1}" ]
+          else
+            scores
+          end
+        end
+      end
+      def score_multiples( dice )
+        if @state == :open and dice == [ 1, 1, 1 ]
+          [ "1000:1,2,3" ]
+        else
+          []
+        end
+      end
+    
   end
 end
